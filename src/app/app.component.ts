154b787961e8f40ce9a79dd8590637fd4e60fe59
oicrruf <vm.reyesal@gmail.com>
2024-08-14 11:15:28 -0600

    chore: home design

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { SwitcherComponent } from './components/switcher/switcher.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,SwitcherComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'jobstack';
+  title = 'Inicio';
 }
--- src/app/pages/index/index-nine/index-nine.component.ts
+++ src/app/pages/index/index-nine/index-nine.component.ts
@@ -1,33 +1,11 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { NavbarComponent } from '../../../components/navbar/navbar.component';
-import { MillionsOfJobComponent } from '../../../components/millions-of-job/millions-of-job.component';
-import { PopularJobFiveComponent } from '../../../components/popular-job-five/popular-job-five.component';
-import { CounterComponent } from '../../../components/counter/counter.component';
-import { CtaComponent } from '../../../components/cta/cta.component';
-import { BestCompaniesTwoComponent } from '../../../components/best-companies-two/best-companies-two.component';
-import { BlogComponent } from '../../../components/blog/blog.component';
-import { FooterComponent } from '../../../components/footer/footer.component';
 
 @Component({
   selector: 'app-index-nine',
   standalone: true,
-  imports: [
-    CommonModule,
-    RouterLink,
-    NavbarComponent,
-    MillionsOfJobComponent,
-    PopularJobFiveComponent,
-    CounterComponent,
-    CtaComponent,
-    BestCompaniesTwoComponent,
-    BlogComponent,
-    FooterComponent
-  ],
+  imports: [RouterLink],
   templateUrl: './index-nine.component.html',
-  styleUrl: './index-nine.component.scss'
+  styleUrl: './index-nine.component.scss',
 })
-export class IndexNineComponent {
-
-}
+export class IndexNineComponent {}
